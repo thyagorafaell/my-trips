75ba6bcdab98d428e7dca2a02bc27e23c043a464
Thyago Rafael <thyagorafael012@gmail.com>
2022-08-13 13:51:16 -0300

    Mapa customizado

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -15,6 +15,27 @@ export type MapProps = {
   places?: Place[]
 }
 
+const MAPBOX_APIKEY = process.env.NEXT_PUBLIC_MAPBOX_APIKEY
+const MAPBOX_USERID = process.env.NEXT_PUBLIC_MAPBOX_USERID
+const MAPBOX_DARKMODE = process.env.NEXT_PUBLIC_MAPBOX_DARKMODE
+
+const CustomTileLayer = () => {
+  if (MAPBOX_APIKEY)
+    return (
+      <TileLayer
+        attribution='&copy; <a href="https://www.app.mapbox.com/feedback">Mapbox</a> contributors &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_DARKMODE}/tiles/256/{z}/{x}/{y}?access_token=${MAPBOX_APIKEY}`}
+      />
+    )
+
+  return (
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
+    />
+  )
+}
+
 const Map = ({ places }: MapProps) => {
   const router = useRouter()
 
@@ -24,10 +45,7 @@ const Map = ({ places }: MapProps) => {
       zoom={3}
       style={{ height: '100%', width: '100%' }}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
-      />
+      {<CustomTileLayer />}
       {places?.map((place: Place) => (
         <Marker
           key={`place-${place.id}`}
