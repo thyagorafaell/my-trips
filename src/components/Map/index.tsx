9e17b06e28e64b0deb9a8322d913e5aca0c2be89
Thyago Rafael <thyagorafael012@gmail.com>
2022-08-13 12:02:19 -0300

    Place page

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 
 type Place = {
@@ -14,24 +15,33 @@ export type MapProps = {
   places?: Place[]
 }
 
-const Map = ({ places }: MapProps) => (
-  <MapContainer
-    center={[0, 0]}
-    zoom={3}
-    style={{ height: '100%', width: '100%' }}
-  >
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
-    />
-    {places?.map((place: Place) => (
-      <Marker
-        key={`place-${place.id}`}
-        position={[place.location.latitude, place.location.longitude]}
-        title={place.name}
+const Map = ({ places }: MapProps) => {
+  const router = useRouter()
+
+  return (
+    <MapContainer
+      center={[0, 0]}
+      zoom={3}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
       />
-    ))}
-  </MapContainer>
-)
+      {places?.map((place: Place) => (
+        <Marker
+          key={`place-${place.id}`}
+          position={[place.location.latitude, place.location.longitude]}
+          eventHandlers={{
+            click() {
+              router.push(`/place/${place.slug}`)
+            }
+          }}
+          title={place.name}
+        />
+      ))}
+    </MapContainer>
+  )
+}
 
 export default Map
--- src/components/templates/Places/index.tsx
+++ src/components/templates/Places/index.tsx
@@ -1,4 +1,8 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+import LinkWrapper from 'components/LinkWrapper'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
+import * as S from './styles'
 
 type ImageProps = {
   url: string
@@ -18,19 +22,36 @@ export type PlacesTemplateProps = {
 }
 
 export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  const router = useRouter()
+
+  if (router.isFallback) return null
+
   return (
     <>
-      <h1>{place.name}</h1>
-      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
-      {place.gallery.map((image) => (
-        <Image
-          key={image.url}
-          alt={place.name}
-          src={image.url}
-          height={image.height}
-          width={image.width}
-        />
-      ))}
+      <LinkWrapper href="/">
+        <CloseOutline size={32} />
+      </LinkWrapper>
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>
+            <h1>{place.name}</h1>
+          </S.Heading>
+          <S.Body>
+            <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+            <S.Gallery>
+              {place.gallery.map((image) => (
+                <Image
+                  key={image.url}
+                  alt={place.name}
+                  src={image.url}
+                  height={image.height}
+                  width={image.width}
+                />
+              ))}
+            </S.Gallery>
+          </S.Body>
+        </S.Container>
+      </S.Wrapper>
     </>
   )
 }
