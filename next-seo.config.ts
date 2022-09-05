8d1916391cb892bf801b6f92c510eb0eb478a178
Thyago Rafael <thyagorafael012@gmail.com>
2022-09-04 22:26:40 -0300

    Limpeza de repositório

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 export default {
 	canonical: 'http://my-trips-green.vercel.app',
 	description: 'Este é o projeto realizado no curso NextJS na prática, de autoria do Willian Justen. O site foi desenvolvido utilizando NextJS e GraphQL - usando a camada free da Hygraph - por Thyago Rafael e publicado na Vercel. Neste site estarão listados alguns destinos que eu e minha família conhecemos ou temos vontade de conhecer :) Veja no Github: https://github.com/thyagorafaell/my-trips.',
@@ -6,11 +7,11 @@ export default {
 		type: 'website',
 		locale: 'pt_BR',
 		url: 'http://my-trips-green.vercel.app',
-		site_name: 'My Trips - thyagorafaell',
+		site_name: 'My Trips - thyagorafaell'
 	},
 	twitter: {
 		handle: '@handle',
 		site: '@site',
-		cardType: 'summary_large_image',
-	},
+		cardType: 'summary_large_image'
+	}
 };
