73ebc9a05c00981a8877ed066d66b6f217bc8b11
Thyago Rafael <thyagorafael012@gmail.com>
2022-09-15 20:01:43 -0300

    Improve sharing

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,17 +1,24 @@
 /* eslint-disable max-len */
+const projectDescription = 'Este é o projeto realizado no curso NextJS na prática, de autoria do Willian Justen. O site foi desenvolvido utilizando NextJS e GraphQL - usando a camada free da Hygraph - por Thyago Rafael e publicado na Vercel. Neste site estarão listados alguns destinos que eu e minha família conhecemos ou temos vontade de conhecer :) Veja no Github: https://github.com/thyagorafaell/my-trips.';
+const projectTitle = 'My Trips - thyagorafaell';
+const projectUrl = 'http://my-trips-green.vercel.app';
+
 export default {
-	canonical: 'http://my-trips-green.vercel.app',
-	description: 'Este é o projeto realizado no curso NextJS na prática, de autoria do Willian Justen. O site foi desenvolvido utilizando NextJS e GraphQL - usando a camada free da Hygraph - por Thyago Rafael e publicado na Vercel. Neste site estarão listados alguns destinos que eu e minha família conhecemos ou temos vontade de conhecer :) Veja no Github: https://github.com/thyagorafaell/my-trips.',
-	title: 'My Trips - thyagorafaell',
+	canonical: projectUrl,
+	description: projectDescription,
+	title: projectTitle,
 	openGraph: {
-		type: 'website',
+		description: projectDescription,
+		image: `${projectUrl}/img/cover.png`,
 		locale: 'pt_BR',
-		url: 'http://my-trips-green.vercel.app',
-		site_name: 'My Trips - thyagorafaell'
+		site_name: projectTitle,
+		title: projectTitle,
+		type: 'website',
+		url: projectUrl
 	},
 	twitter: {
+		cardType: 'summary_large_image',
 		handle: '@handle',
-		site: '@site',
-		cardType: 'summary_large_image'
+		site: '@site'
 	}
 };
